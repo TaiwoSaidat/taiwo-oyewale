ff65f5e4ea1b49fcfc46e356ac4f1871eae3a058
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-09-29 16:31:38 +0100

    chore/favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Taiwo Oyewale | Frontend Software Engineer",
   description: "My Portfolio Website",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
